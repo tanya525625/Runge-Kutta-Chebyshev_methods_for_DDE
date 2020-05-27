083e21203f1870989315cfcaeb5e7590af8e9547
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2020-05-27 15:44:27 +0300

    fixed interpolation

--- history_func.m
+++ history_func.m
@@ -1,4 +1,4 @@
 function dy = history_func(t)
-    dy(1) = exp(t);
+    dy(1) = exp(-t);
 %     dy(2) = sin(t);
 end
--- main_DDE.m
+++ main_DDE.m
@@ -6,25 +6,25 @@ history_func_ = @history_func;
 delayed_func_ = @scalar_DDE;
 t_retarded = 1; 
 args_count = 1;
-h = 1;
-count = 100;
+h = 0.01;
+count = 10000;
 span_end = h * count;
+s = 5;
+koef = 0;
+lambda = -1700;
+is_inter = 1;
+mu = 200;
 
+% создание объекта класса для решения ДУЗА 
 diffEq1 = SolveDDE(span_start, span_end, count, t_retarded,...
                    approx_init, args_count,...
                    delayed_func_, history_func_);
                     
-s = 3;
-koef = 0;
-lambda = -500;
-is_inter = 0;
-mu = 0;
-%h = (span_end - span_start) / count
-
 method = 'RKC for scalar equation: ';
 title = strcat(method, 32, 'N =', 32, num2str(count), ', s =', 32, num2str(s),... 
         ', h =', 32, num2str(h), ', lambda =', 32, num2str(lambda), ', mu =', 32, num2str(mu));
-                  
-dy1 = diffEq1.RKC_for_scalar_DDE(s, koef, is_inter);
-show_plots_for_method(diffEq1.timespan, dy1, title)
 
+% нахождение численного решения
+dy1 = diffEq1.RKC_for_scalar_DDE(s, koef, is_inter);
+% построение графиков численного решения
+show_plots_for_method(diffEq1.timespan, dy1, title)
